c23bdf326a87c9d4c38ed2fadec415487194ea66
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2025-06-02 13:34:31 +0200

    Refactor layout structure and styling
    Replaced `+page.svelte` with a new `+layout.svelte` to establish a site-wide layout and navigation. Added basic styling, a "Hej" header, and support for rendering child components dynamically.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+
+ let { children } = $props();
+
+</script>
+
+<nav>
+ <h1>Hej</h1>
+</nav>
+
+ <div>
+  {@render children()}
+ </div>
+
+<style>
+ :root {
+  font-family: "Helvetica Neue", sans-serif;
+  color: #21130d;
+ }
+ nav {
+  z-index: 999;
+ }
+</style>
