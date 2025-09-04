cb2c3be5b7d84244892cf9918ba916e9f4c55805
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2025-09-04 14:03:26 +0200

    Update `FilterList.svelte`: integrate `toggleFilter` for filter management, connect to query state, and adjust styling.

--- src/lib/client/components/FilterList.svelte
+++ src/lib/client/components/FilterList.svelte
@@ -1,6 +1,11 @@
 <script>
+    import { getContext } from "svelte";
+    import { CONTEXT } from "$lib/utils/constants.js";
+    import { addFilter, removeFilter } from "$lib/utils/helpers.js";
+
     let {
-        facetOptions
+        facetOptions,
+        type
     } = $props();
 
     let limit = $state(10);
@@ -11,17 +16,29 @@
             res.push(facetOptions[i]);
         }
         return res;
-    })
+    });
 
-    function applyFilter() {}
+    let queryState = getContext(CONTEXT.QUERY_STATE);
+
+    function toggleFilter(key) {
+        /** @type Filter */ let filter = {
+            type,
+            value: key
+        }
+        // console.log('adding filter: ' + filter.value)
+
+        addFilter(queryState.filters, filter);
+
+        // console.log($state.snapshot(queryState.filters));
+    }
 </script>
 
 <div>
         {#each displayOptions as opt}
             <div class="filter-element">
-                <input type="checkbox" />
+                <input type="checkbox" onchange={() => toggleFilter(opt.key)} />
                 <span>{opt.key}</span>
-                <span class="text-count">{opt.count}</span>
+                <span class="text-count small">{opt.count}</span>
             </div>
         {/each}
     {#if facetOptions.length > limit}
@@ -32,7 +49,7 @@
 <style>
     .filter-element {
         display: flex;
-        gap: 20px;
+        gap: 10px;
     }
 
     .text-count {
