fe35f070910e512e348de4d8caa6541586916682
Bjarke Vognstrup Fog <bfog@cc.au.dk>
2025-10-10 13:33:33 +0200

    Remove unused typedefs and helper functions; rename context key `GRAPH_STATE` to `GRAPH_VIEW_STATE`.

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -1,5 +1,5 @@
 export const CONTEXT = {
-  GRAPH_STATE: 'graphState',
+  GRAPH_STATE: 'graphViewState',
   QUERY_STATE: 'queryState',
   FACETS: 'facets',
   RESOLVE_URL: 'resolveURL'
--- src/lib/utils/helpers.js
+++ src/lib/utils/helpers.js
@@ -1,38 +1,15 @@
-/**
- * Function for adding filters to an array
- * @param {Filter[]} arr
- * @param {Filter} filter
- */
-export function addFilter(arr, filter) {
-    if (!hasFilter(arr, filter)) {
-        arr.push(filter);
-    }
-}
 
-/**
- * Function for removing filters from an array
- * @param {Filter[]} arr
- * @param {Filter} filter
- */
-export function removeFilter(arr, filter) {
-    if (hasFilter(arr, filter)) {
-        let index = arr.findIndex(el => el.type === filter.type && el.value === filter.value);
-        arr.splice(index, 1);
-    }
-}
-
-/**
- * Whether the filter is in the array
- * @param {Filter[]} arr
- * @param {Filter} filter
- * @return {*}
- */
-function hasFilter(arr, filter) {
-    return arr.some(el => {
-        return el.type === filter.type && el.value === filter.value;
-    });
-}
 
 export function isNotNullNotEmptyString(str) {
     return str && typeof str === "string" && str.length > 0;
+}
+
+/**
+ *
+ * @param {string} str
+ * @param {string} searchStr
+ * @return {boolean}
+ */
+export function stringContainsIgnoreCase(str, searchStr) {
+    return str.toLowerCase().includes(searchStr.toLowerCase());
 }
