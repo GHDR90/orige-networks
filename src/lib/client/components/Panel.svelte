32501b60a64f09bef83f3f3cc53e8edfea1d7f33
Bjarke Vognstrup Fog <bfog@cc.au.dk>
2025-10-29 14:27:44 +0100

    Add filter mode radio options to `Panel.svelte` for managing query state

--- src/lib/client/components/Panel.svelte
+++ src/lib/client/components/Panel.svelte
@@ -1,22 +1,55 @@
 <script>
+    import { getContext } from "svelte";
+    import { CONTEXT } from "$lib/utils/constants.js";
+
     let {
         header,
         isCollapsible = true,
         startCollapsed = true,
+        isFilterPanel = false,
+        filterOptions = {},
         children
     } = $props();
+
+    let queryState = getContext(CONTEXT.QUERY_STATE);
 
     let isCollapsed = $state(isCollapsible && startCollapsed);
 
     function toggleCollapse() {
         isCollapsed = !isCollapsed;
     }
+
+    function getIDForPanel(str) {
+        return str.split(" ")[0].toLowerCase();
+    }
+
+    const titleTextAny = "Match any of the selected filters in the list";
+    const titleTextAll = "Match all selected filters in the list";
+
 </script>
 
 <div class="panel">
     <div class="panel-header">
         {#if header.length > 0}
         <h2>{header}</h2>
+        {/if}
+        {#if isFilterPanel && filterOptions.filterModes.length > 0}
+            <div class="radio-mode-container">
+            {#each filterOptions.filterModes as mode}
+                <div>
+                    <label for="radio-{mode}-{filterOptions.filterType}">{mode}</label>
+                    <input
+                            disabled={!filterOptions.filterModes.some(m => m === mode)}
+                            title={mode === 'any' ? titleTextAny : titleTextAll}
+                            value={mode}
+                            bind:group={queryState.filterModes[filterOptions.filterType]}
+                            type="radio"
+                            id="radio-{mode}-{filterOptions.filterType}"
+                            name="any-all-{filterOptions.filterType}"
+                    />
+                </div>
+            {/each}
+            </div>
         {/if}
         {#if isCollapsible}
             <button
